59d0f371a98f340a48282193435e76f279d2f90a
charly philippe <charly.ph@hotmail.fr>
2022-08-15 01:20:14 +0200

    That took me some time

---
+++ script.js
@@ -0,0 +1,45 @@
+const dogUrl = "https://dog.ceo/api/breed/";
+const catUrl = "https://api.thecatapi.com/v1/images/search?breed_ids="
+const dogs = "";
+const cats = "";
+let audio1 = new Audio("/sounds/mixkit-armhole-farting-3051.wav");
+let audio2 = new Audio("/sounds/VOXScrm_Wilhelm scream (ID 0477)_BSB.wav");
+// Doggo Fetcher
+
+const fetchDogos = async () => {
+  let input = document.getElementById("dog");
+  const urlToFetch = dogUrl + input.value + "/images";
+  const response = await fetch(urlToFetch);
+  if (response.ok) {
+    const jsonResponse = await response.json();
+    const dogs = jsonResponse.message;
+    const random = Math.floor(Math.random() * dogs.length);
+    document.getElementById("chien").src = dogs[random];
+    return dogs;
+  }
+};
+
+// Catto Fetcher
+
+const fetchCatos = async () => {
+  let input = document.getElementById("cat");
+  const urlToFetch = catUrl + input.value;
+  const response = await fetch(urlToFetch);
+  if (response.ok) {
+    const jsonResponse = await response.json();
+    console.log(jsonResponse)
+    const cats = jsonResponse
+    const random = Math.floor(Math.random() * cats.length);
+    document.getElementById("chat").src = cats[random].url;
+    return cats;
+  }
+};
+
+// Classy functions
+const FartyBoy = function(){
+audio1.play();
+};
+
+const Whilelm = function(){
+  audio2.play();
+  };
